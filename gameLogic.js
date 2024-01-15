2ba5181d306cc053444e8db40148add9420ba1a0
Lukas Kokerle <lukas.kokerle@gmx.at>
2024-01-15 11:42:17 +0100

    Create game loop for 5-round game against computer

--- gameLogic.js
+++ gameLogic.js
@@ -1,3 +1,5 @@
+console.log(game());
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
     console.log(choice);
@@ -11,5 +13,49 @@ function getComputerChoice() {
             return "Scissors";
         default:
             return "Error";
+    }
+}
+
+function turn(playerSelection, computerSelection) {
+    if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
+        (playerSelection == "Paper" && computerSelection == "Rock") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper")) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if ((playerSelection == "Paper" && computerSelection == "Scissors") ||
+               (playerSelection == "Rock" && computerSelection == "Paper") ||
+               (playerSelection == "Scissors" && computerSelection == "Rock")) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection == computerSelection) {
+        return "Draw!";
+    }
+}
+
+function game() {
+    let i = 0;
+    let playerWin = 0;
+    let computerWin = 0;
+    while (i < 5) {
+        const playerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors':");
+        const computerSelection = getComputerChoice();
+        let result = turn(playerSelection, computerSelection);
+        console.log(result);
+
+        if (result.includes("win")) {
+            playerWin++;
+        } else if (result.includes("lose")) {
+            computerWin++;
+        }
+        i++;
+    }
+    return determineWinner(playerWin, computerWin);
+}
+
+function determineWinner(playerWin, computerWin) {
+    if (playerWin > computerWin) {
+        return "The winner of the game is you!";
+    } else if (playerWin < computerWin) {
+        return "The winner of the game is the computer!";
+    } else if (playerWin == computerWin) {
+        return "The game is a draw!";
     }
 }
